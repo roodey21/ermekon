d3b2c3949a8017dacdceb9ca0599229391750ac5
roodey21 <fahrudin.abadi@students.amikom.ac.id>
2023-10-24 10:35:48 +0700

    add dasboard page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,128 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { BanknotesIcon, CheckBadgeIcon, UserGroupIcon, WalletIcon } from '@heroicons/vue/24/outline';
 import { Head } from '@inertiajs/vue3';
+
+defineOptions({
+    layout: AuthenticatedLayout,
+})
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+    <div class="px-4 py-6">
+        <div class="grid grid-cols-4 gap-4">
+            <div class="flex justify-between p-4 bg-white border rounded-lg shadow-sm">
+                <div class="flex items-center">
+                    <div class="flex items-center justify-center p-3 bg-teal-500 rounded-xl">
+                        <WalletIcon class="w-5 h-5 text-white" />
+                    </div>
+                    <div class="ml-4">
+                        <div class="text-[10px] font-medium text-gray-500">Total</div>
+                        <div class="text-sm font-semibold text-gray-900">Project</div>
+                    </div>
+                </div>
+                <div class="flex flex-col self-center text-right">
+                    <div class="text-lg font-semibold text-gray-900">16</div>
+                    <div class="text-[10px] font-medium text-gray-500">18% completd</div>
+                </div>
+            </div>
+            <div class="flex justify-between p-4 bg-white border rounded-lg shadow-sm">
+                <div class="flex items-center">
+                    <div class="flex items-center justify-center p-3 rounded-xl bg-sky-500">
+                        <CheckBadgeIcon class="w-5 h-5 text-white" />
+                    </div>
+                    <div class="ml-4">
+                        <div class="text-[10px] font-medium text-gray-500">Total</div>
+                        <div class="text-sm font-semibold text-gray-900">Tasks</div>
+                    </div>
+                </div>
+                <div class="flex flex-col self-center text-right">
+                    <div class="text-lg font-semibold text-gray-900">40</div>
+                    <div class="text-[10px] font-medium text-gray-500">12% completd</div>
+                </div>
+            </div>
+            <div class="flex justify-between p-4 bg-white border rounded-lg shadow-sm">
+                <div class="flex items-center">
+                    <div class="flex items-center justify-center p-3 bg-rose-500 rounded-xl">
+                        <BanknotesIcon class="w-5 h-5 text-white" />
+                    </div>
+                    <div class="ml-4">
+                        <div class="text-[10px] font-medium text-gray-500">Total</div>
+                        <div class="text-sm font-semibold text-gray-900">Expense</div>
+                    </div>
+                </div>
+                <div class="flex flex-col self-center text-right">
+                    <div class="text-lg font-semibold text-gray-900">16</div>
+                    <div class="text-[10px] font-medium text-gray-500"></div>
+                </div>
+            </div>
+            <div class="flex justify-between p-4 bg-white border rounded-lg shadow-sm">
+                <div class="flex items-center">
+                    <div class="flex items-center justify-center p-3 bg-amber-500 rounded-xl">
+                        <UserGroupIcon class="w-5 h-5 text-white" />
+                    </div>
+                    <div class="ml-4">
+                        <div class="text-[10px] font-medium text-gray-500">Total</div>
+                        <div class="text-sm font-semibold text-gray-900">Customers</div>
+                    </div>
+                </div>
+                <div class="flex flex-col self-center text-right">
+                    <div class="text-lg font-semibold text-gray-900">9</div>
+                    <div class="text-[10px] font-medium text-gray-500"></div>
+                </div>
+            </div>
+            <div class="bg-white border rounded-lg shadow-sm">
+                <div class="py-4 border-b">
+                    <div class="px-4 border-l-2 border-teal-600">
+                        <h5 class="text-base font-medium">Project Status</h5>
+                    </div>
+                </div>
+                <div class="p-4">
+                    <div class="grid grid-cols-2 gap-x-6 gap-y-8">
+                        <div>
+                            <span class="block text-xs font-light text-gray-600">In Progress</span>
+                            <div class="text-xl font-semibold text-sky-500">
+                                5%
+                            </div>
+                            <div class="w-full h-1.5 bg-gray-200 rounded relative overflow-hidden shadow">
+                                <div class="absolute inset-y-0 w-4 bg-sky-500"></div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="block text-xs font-light text-gray-600">On Hold</span>
+                            <div class="text-xl font-semibold text-amber-500">
+                                5%
+                            </div>
+                            <div class="w-full h-1.5 bg-gray-200 rounded relative overflow-hidden shadow">
+                                <div class="absolute inset-y-0 w-4 bg-amber-500"></div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="block text-xs font-light text-gray-600">Complete</span>
+                            <div class="text-xl font-semibold text-teal-500">
+                                3%
+                            </div>
+                            <div class="w-full h-1.5 bg-gray-200 rounded relative overflow-hidden shadow">
+                                <div class="absolute inset-y-0 w-2 bg-teal-500"></div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="block text-xs font-light text-gray-600">Canceled</span>
+                            <div class="text-xl font-semibold text-rose-500">
+                                3%
+                            </div>
+                            <div class="w-full h-1.5 bg-gray-200 rounded relative overflow-hidden shadow">
+                                <div class="absolute inset-y-0 w-2 bg-rose-500"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </AuthenticatedLayout>
+        <!-- <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">You're logged in!</div>
+        </div> -->
+    </div>
 </template>
